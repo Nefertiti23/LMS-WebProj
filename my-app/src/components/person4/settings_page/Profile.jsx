a1427b722e129ada4e789231862acb7197d341cf
Nefertiti23 <fatima.xx.umar@gmail.com>
2025-11-15 16:56:13 +0500

    complete profile settings

--- my-app/src/components/person4/settings_page/Profile.jsx
+++ my-app/src/components/person4/settings_page/Profile.jsx
@@ -3,8 +3,8 @@ import './Profile.css'
 
 export default function Profile ({user}) {
   const [theuser, setUser] = useState(user);
-
-  const [formDisplay, setDisplay] = useState('none')
+  const [formDisplay, setDisplay] = useState('none');
+  const [formImg, setImg] = useState(theuser.imageURL);
 
   const showForm = () => {
     setDisplay('flex');
@@ -15,24 +15,33 @@ export default function Profile ({user}) {
     if (name === '') alert("Please enter a name");
     else {
       setUser(prevuser => {
-        return {...prevuser, name: name}
+        return {...prevuser, name: name, imageURL: formImg}
       });
-
-      setDisplay('none')
+      setDisplay('none');
     }
   }
 
   return (
     <>
     <div className="profileScreen">
-      {/* form to edit profile info */}
-      <form className='fixed' style={{display: formDisplay}}>
-        <div className="inputbox">
-          <label>Display name</label>
-          <input id="newname" type='text' defaultValue={theuser.name} required></input>
-        </div>
-        <button type='button' onClick={updateUser}>Confirm</button>
-      </form>
+      {/* form to edit profile info h-screen bg-stone-500/75 fixed z-100 boder-2 border-amber-300*/}
+      <div className='formBg' style={{display: formDisplay}}>
+        <form className='rounded-md'>
+            <div>
+              <img id='selectedIMG' src={formImg} className='border-2 border-slate-700 my-4' onClick={() => document.getElementById('fileInput').click()} alt="Click to upload"></img>
+              <p className='text-sm text-slate-500'>Click avatar to change</p>
+              <input id='fileInput' type='file' accept ="image/jpeg, image/png, image/jpg" className='hidden' onChange={(e) => setImg(URL.createObjectURL(e.target.files[0]))}></input>
+            </div>
+          <div className="flex flex-col gap-2 my-4">
+            <label className='font-medium'>Display name</label>
+            <input className='rounded-md' id="newname" type='text' defaultValue={theuser.name} required></input>
+          </div>
+          <div className='buttonbox'>
+            <button id="cancel" type='button' onClick={() => setDisplay('none')}>Cancel</button>
+            <button id="confirm" type='button' onClick={updateUser}>Confirm</button>
+          </div>
+        </form>
+      </div>
 
       <h3 className='font-semibold text-3xl'>Profile</h3>
 
