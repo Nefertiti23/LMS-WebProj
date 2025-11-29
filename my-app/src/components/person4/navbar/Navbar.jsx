e32aac2973a949d06693e7aa124c383b607b7731
Nefertiti23 <fatima.xx.umar@gmail.com>
2025-11-29 11:43:15 +0500

    Some palette fixes

--- my-app/src/components/person4/navbar/Navbar.jsx
+++ my-app/src/components/person4/navbar/Navbar.jsx
@@ -4,26 +4,26 @@ export default function SideNav() {
   return (
     <nav className='flex flex-col gap-3 w-64 p-4'>
         <div className='flex flex-col gap-1'>
-            <Link to='/dashboard' className='hover:bg-slate-900 px-2 py-1.5 rounded text-sm font-light'>
+            <Link to='/dashboard' className='link px-2 py-1.5 rounded text-sm font-light'>
             Dashboard
             </Link>
             <p className='m-1 text-sm font-extralight'>MANAGE</p>
-            <Link to='/profile' className='hover:bg-slate-900 px-2 py-1.5 rounded text-sm font-light'>
+            <Link to='/profile' className='link px-2 py-1.5 rounded text-sm font-light'>
             Profile
             </Link>
-            <Link to='/accountinfo' className='hover:bg-slate-900 px-2 py-1.5 rounded text-sm font-light'>
+            <Link to='/accountinfo' className='link px-2 py-1.5 rounded text-sm font-light'>
             Account
             </Link>
-            <Link to='/billing' className='hover:bg-slate-900 px-2 py-1.5 rounded text-sm font-light'>
+            <Link to='/billing' className='link px-2 py-1.5 rounded text-sm font-light'>
             Billing
             </Link>
-            <Link to='/progressreps' className='hover:bg-slate-900 px-2 py-1.5 rounded text-sm font-light'>
+            <Link to='/progressreps' className='link px-2 py-1.5 rounded text-sm font-light'>
             Progress
             </Link>
-            <Link to='/certificates' className='hover:bg-slate-900 px-2 py-1.5 rounded text-sm font-light'>
+            <Link to='/certificates' className='link px-2 py-1.5 rounded text-sm font-light'>
             Certificates
             </Link>
-            <Link to='/notifs' className='hover:bg-slate-900 px-2 py-1.5 rounded text-sm font-light'>
+            <Link to='/notifs' className='link px-2 py-1.5 rounded text-sm font-light'>
             Notifications
             </Link>
         </div>
